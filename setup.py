fa216178404c3af7305a38bbf519b006c130750a
Ondrej Chvala <ondrejch@gmail.com>
2025-05-11 16:38:42 -0400

    in progress

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='VR1 in OpenMC',
+    version='0.01',
+    packages=['vr1'],
+    url='',
+    license='MIT',
+    author='Ondrej Chvala, Alex Macris',
+    author_email='ochvala@utexas.edu',
+    description='OpenMC model of VR-1 reactor Vrabec'
+)
